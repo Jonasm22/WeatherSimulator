3ff9079c93cb7ecfe5be3896cd205bb853a0cf5a
Jonathan Mendoza <john.2114@hotmail.com>
2024-07-11 11:54:32 +0200

    End_WeatherSimulator_Demo

--- WeatherSimulator/Program.cs
+++ WeatherSimulator/Program.cs
@@ -16,8 +16,8 @@
         for (int i = 0; i < days; i++)
         {
             temperature[i] = random.Next(-10, 40);
-           weatherConditions[i] = conditions[random.Next(conditions.Length)];
-            
+            weatherConditions[i] = conditions[random.Next(conditions.Length)];
+
         }
 
 
@@ -26,45 +26,80 @@
         Console.WriteLine($"Avare Temperature {avarageTemp}");
         Console.WriteLine($" the max temp was : {temperature.Max()}");
         Console.WriteLine($" the min temp was : {temperature.Min()}");
-       // Console.WriteLine($" the min temp was : {MinTemperature(temperature)}");
-       
+        Console.WriteLine($" The most common Condition : {MostCommonCondition(weatherConditions)}");
+
+        // Console.WriteLine($" the min temp was : {MinTemperature(temperature)}");
+
         Console.ReadKey();
+
     }
 
 
-    static double CalculateAvarage(int[] temperature)
+    static string MostCommonCondition(string[] conditions)// name
     {
-        double sum = 0;
+        int count = 0;
 
-        for (int i = 0; i < temperature.Length; i++)
+        string mostCommon = conditions[0];
+
+
+        for (int i = 0; i < conditions.Length; i++)
         {
-            sum += temperature[i];
-        }
-
-        // temperature.Length is the amount of items inside of temperature
-        var avarage = sum / temperature.Length;
-
-        return avarage;
-    }
+            int tempCount = 0;
 
 
-    // calculate Optional code
-    static int MinTemperature(int[] temperature)
-    {
-        int min = temperature[0];
-
-        foreach(int temp in temperature)
-        {
-            if( temp < min)
+            for (int j = 0; j < conditions.Length; j++)
             {
-                min = temp; 
-            }
-        
-        }
-         return min;    
+                if (conditions[j] == conditions[i])
+                {
+                    tempCount++;
+                }
 
+                if (tempCount > count)
+                {
+                    mostCommon = conditions[i];
+                }
+
+
+            }
+
+        }
+        return mostCommon;
 
     }
 
+        static double CalculateAvarage(int[] temperature)
+        {
+            double sum = 0;
 
-}
+            for (int i = 0; i < temperature.Length; i++)
+            {
+                sum += temperature[i];
+            }
+
+            // temperature.Length is the amount of items inside of temperature
+            var avarage = sum / temperature.Length;
+
+            return avarage;
+        }
+
+
+        // calculate Optional code
+        static int MinTemperature(int[] temperature)
+        {
+            int min = temperature[0];
+
+            foreach (int temp in temperature)
+            {
+                if (temp < min)
+                {
+                    min = temp;
+                }
+
+            }
+            return min;
+
+
+        }
+
+
+    }
